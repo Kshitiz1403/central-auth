4d0db990f2618da27f803ee14a507e1831566c68
Kshitiz Agrawal <kshitizagrawal@outlook.com>
2024-10-20 16:23:30 +0530

    fix: change in logic for generating secrets

--- authentik/1-generate-password.sh
+++ authentik/1-generate-password.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 echo "PG_PASS=$(openssl rand -base64 36 | tr -d '\n')" >> .env
-echo "AUTHENTIK_SECRET_KEY=$(openssl rand -base64 60 | tr -d '\n')" >> .env
+echo "AUTHENTIK_SECRET_KEY=$(pwgen -s 40 1  | tr -d '\n')" >> .env
